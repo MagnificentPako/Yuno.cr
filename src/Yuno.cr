830f52105ae565bc6a16999c532aaf59ac631113
Paul <paul@niobium.me>
2017-05-26 16:45:24 +0200

    Added ready message

--- src/Yuno.cr
+++ src/Yuno.cr
@@ -8,5 +8,12 @@ module Yuno
   BOT = Ctx::Bot.new(token: ENV["TOKEN"],
     client_id: 301679727540699136_u64)
   SELF = Ctx::Context.message_create { |m| m.author.id == 139395104455524352 }
+  
+  Bot.on_ready do |payload|
+    puts "================"
+    puts "REEEEEEEEEEEEEEE"
+    puts "================"
+  end
+  
   BOT.run
 end
