e25b7d7cf61b4e19a86191bc8801fbdf89a4f4e5
Paul <paul@niobium.me>
2017-07-18 00:42:42 +0200

    [Added] fuckton of waving kaomoji

--- src/Yuno/commands/dongers.cr
+++ src/Yuno/commands/dongers.cr
@@ -1,4 +1,18 @@
+require "json"
+
 module Yuno
+
+  wave = {} of String => Array(String)
+  File.open("../wave.json", "r") do |io|
+    while line = io.gets
+      wave = Hash(String, Array(String)).from_json(line) 
+    end
+  end
+     
+  BOT.command("~~wave", [SELF]) do |payload, args|
+    BOT.edit_message(payload.channel_id, payload.id, wave.values.sample.sample)
+  end
+
   BOT.command("~~lenny", [SELF]) do |payload, args|
     BOT.edit_message(payload.channel_id, payload.id, LENNY.sample)
   end
