fe83862dde0a4222e7242f7ec7d2b28f4a75a7f0
Paul <paul@niobium.me>
2017-07-21 20:34:26 +0200

    Fixed fix of fix

--- src/Yuno/commands/urn.cr
+++ src/Yuno/commands/urn.cr
@@ -1,6 +1,6 @@
 module Yuno
   BOT.command("~~urn", [SELF]) do |payload, arguments|
-    proc = Process.run(command : String, input = IO::Memory.new(arguments), output = true)
+    proc = Process.run(command = "urn", args = "--exec", input = IO::Memory.new(arguments), output = true)
     output = proc.output_gets_to_end
     status = proc.await
     puts status
