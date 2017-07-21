422e9d8eb0d40ab0b2f7725f38777265ab50a659
Paul <paul@niobium.me>
2017-07-21 21:29:28 +0200

    Added mutype command

---
+++ src/Yuno/commands/mutype.cr
@@ -0,0 +1,21 @@
+module Yuno
+  BOT.command("~~mutype", [SELF]) do |payload, arguments|
+    io = IO::Memory.new
+    status = Process.run(command: "/home/paul/.cabal/bin/mueval", args: ["-iTe", arguments], input: IO::Memory.new(arguments), output: io)
+    em = Discord::Embed.new(
+        title: "μtypes🌈",
+        colour: status.exit_status == 0 ? 5025616_u32 : 16007990_u32,
+        fields: [
+            Discord::EmbedField.new(
+                name: "Input:",
+                value: "```haskell\n#{arguments}\n```"
+            ),
+            Discord::EmbedField.new(
+                name: "Output:",
+                value: "```haskell\n#{io.to_s}\n```"
+            )
+        ]
+    )
+    BOT.edit_message(payload.channel_id, payload.id, "", embed: em)
+  end
+end
