f7838ad3039f3b1dbbfc1990f48b5c3e9ffc55ad
Paul <paul@niobium.me>
2017-07-21 20:31:32 +0200

    Fixed mistake in urn command

--- src/Yuno/commands/urn.cr
+++ src/Yuno/commands/urn.cr
@@ -1,6 +1,6 @@
 module Yuno
-  BOT.command("~~urn", [SELF]) do |payload, args|
-    proc = Process.run(command : String, args = nil, input = IO::Memory.new(args.join " "), output = true)
+  BOT.command("~~urn", [SELF]) do |payload, arguments|
+    proc = Process.run(command : String, input = IO::Memory.new(arguments.join " "), output = true)
     output = proc.output_gets_to_end
     status = proc.await
     puts status
