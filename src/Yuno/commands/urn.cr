3ab58664032ff1a32c12d87b64d2140fcf8f9347
Paul <paul@niobium.me>
2017-07-21 21:07:42 +0200

    Fixed urn, finally. I hope

--- src/Yuno/commands/urn.cr
+++ src/Yuno/commands/urn.cr
@@ -1,8 +1,21 @@
 module Yuno
   BOT.command("~~urn", [SELF]) do |payload, arguments|
     io = IO::Memory.new
-    status = Process.run(command: "urn", args: ["--exec"], input: IO::Memory.new(arguments), output: io)
-    puts status.exit_status
-    BOT.edit_message(payload.channel_id, payload.id, io.to_s)
+    status = Process.run(command: "urn | stripcolorcodes", args: ["--exec"], shell: true, input: IO::Memory.new(arguments), output: io)
+    em = Discord::Embed.new(
+        title: "Urn🔥",
+        color: 5025616,
+        fields: [
+            Discord::EmbedField.new(
+                name: "Input:",
+                value: "```lisp\n#{arguments}\n```"
+            ),
+            Discord::EmbedField.new(
+                name: "Output:",
+                value: "```lisp\n#{io.to_s}\n```"
+            )
+        ]
+    )
+    BOT.edit_message(payload.channel_id, payload.id, "", em)
   end
 end
