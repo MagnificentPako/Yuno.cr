915ec23ffb18b66bb8c14f4a249016b6cc6e5b32
Paul <paul@niobium.me>
2017-07-26 22:28:45 +0200

    Added some stuff

--- src/Yuno/commands/urn.cr
+++ src/Yuno/commands/urn.cr
@@ -19,7 +19,7 @@ module Yuno
     BOT.edit_message(payload.channel_id, payload.id, "", embed: em)
   end
   `rm -rf /yuno/urn_std`
-  'git clone https://github.com/MagnificentPako/YunoUrnLibs.git /yuno/urn_std'
+  `git clone https://github.com/MagnificentPako/YunoUrnLibs.git /yuno/urn_std`
   BOT.command("~~update-urn", [SELF]) do |payload, arguments|
     em = Discord::Embed.new(
         title: "Urn🔥",
--- urn_std/yuno.lisp
+++
@@ -1,8 +0,0 @@
-(defmacro λ (&args) `(lambda ,@args))
-
-(defun repeat (x y)
-    (if (= 0 y)
-        '()
-        (cons x
-            (repeat x (- y 1)))))
-            
